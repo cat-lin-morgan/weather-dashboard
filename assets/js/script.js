8f3d1fd8c9bd3566d33b9939a713fa7acfebb2f2
cat - lin <plummorgan@gmail.com>
2020-09-11 23:48:09 -0700

    added base html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
 var cityName = document.querySelector("#cityName")
 
 
-//get api info for current weather
-var getCityName = function () {
+//get user's city/location
+var getCityName = function (cityName) {
     var openWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=552d4c7d171826087d706ba7bce893dc";
     //making a request to get the api's info
     fetch(openWeatherApi)
-}
+        .then(function(response) {
+            if (response.ok) {
+                response.json()
+            }
+        })
+        console.log(openWeatherApi);
+    //click event that sends input on button click to this var
+};
+
+getCityName("hollywood");
 
 
-//get api info for 5 day forecast
+
+//put into 5 day area
+
+//put into current weather area 
+
+//have function that saves previously called cities
+
+//have function that loads previously called cities
+
