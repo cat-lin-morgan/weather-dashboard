6640f8bcbde92f13de74bd9acefad49898ef620d
cat - lin <plummorgan@gmail.com>
2020-09-12 00:45:27 -0700

    added imperial units and added button usage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,56 @@
-var cityName = document.querySelector("#cityName")
+var htmlInput = document.querySelector("#cityName"); //input
+var citySearch = document.querySelector("#citySearch"); //button
 
-
-//get user's city/location
-var getCityName = function (cityName) {
-    var openWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=552d4c7d171826087d706ba7bce893dc";
+//get user's current weather from api
+var getCurrentWeather = function (cityName) {
+    var openWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=552d4c7d171826087d706ba7bce893dc";
     //making a request to get the api's info
     fetch(openWeatherApi)
         .then(function(response) {
             if (response.ok) {
-                response.json()
+                response.json().then(function(city) {
+                    console.log(city)
+                });
             }
-        })
-        console.log(openWeatherApi);
-    //click event that sends input on button click to this var
+        });
+        
 };
 
-getCityName("hollywood");
+//get forecast of five from api
+var getFiveCast = function (cityName) {
+    var openWeatherApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=552d4c7d171826087d706ba7bce893dc";
+    //making a request to get the api's info
+    fetch(openWeatherApi)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(city) {
+                    console.log(city)
+                });
+            }
+        });
+};
+
+//get value from input and pass it to getCityWeather
+var weatherInput = function () {
+    var cityName = htmlInput.value;
+    handleSearch(cityName);
+}
+
+//master event that triggers everything else
+var handleSearch = function (cityName) {
+
+//get current weather
+    getCurrentWeather(cityName);
+//get forecast
+    getFiveCast(cityName)
+//display both
+//add current city into local storage
+//display current city in history sidebar
+}
 
 
+var startApplication = function () {
+    //load history from storage and display it
+}
 
-//put into 5 day area
-
-//put into current weather area 
-
-//have function that saves previously called cities
-
-//have function that loads previously called cities
-
+citySearch.addEventListener("click", weatherInput);
